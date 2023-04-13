9d1e4b0b631e22ad699d07d6dffd130a32b86077
Shivam001 <cocbrother001@gmail.com>
2023-04-14 02:09:24 +0530

    first commit

---
+++ src/main/java/Fitternity/divyanshu/DivyanshuApplication.java
@@ -0,0 +1,13 @@
+package Fitternity.divyanshu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivyanshuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivyanshuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Fitternity/divyanshu/DivyanshuApplicationTests.java
@@ -0,0 +1,13 @@
+package Fitternity.divyanshu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivyanshuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
