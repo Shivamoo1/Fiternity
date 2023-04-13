515dd4c3676d0304c22880371a4637f13b3ddb8b
Madhavv024 <madhavvarjuntthukral@gmail.com>
2023-04-14 00:06:21 +0530

    First commit

---
+++ src/test/java/Fitternity/madhavv/FitternityApplicationTests.java
@@ -0,0 +1,13 @@
+package Fitternity.madhavv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitternityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
