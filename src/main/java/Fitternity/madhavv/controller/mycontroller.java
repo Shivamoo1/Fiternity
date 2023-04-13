71c21e6d712408bc08c129d6836bd629953ed1f7
Madhavv024 <madhavvarjuntthukral@gmail.com>
2023-04-14 00:42:50 +0530

    first commit

--- src/main/java/Fitternity/madhavv/controller/mycontroller.java
+++ src/main/java/Fitternity/madhavv/controller/mycontroller.java
@@ -2,15 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package fitternity2.madhavv.controller;
+package Fitternity.madhavv.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-/**
- *
- * @author hp
- */
 @Controller
 public class mycontroller {
 
